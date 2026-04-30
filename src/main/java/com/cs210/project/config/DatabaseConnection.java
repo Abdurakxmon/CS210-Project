b9c711c381c760ebacc1f76c6a0e7ccb84770c01
Abdurakxmon <77540298+Abdurakxmon@users.noreply.github.com>
2026-04-29 23:58:20 -0700

    Create DatabaseConnection.java

---
+++ src/main/java/com/cs210/project/config/DatabaseConnection.java
@@ -0,0 +1,35 @@
+package com.cs210.project.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * DatabaseConnection class to manage JDBC connections to MySQL.
+ * Designed for a CS210 project level.
+ */
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/car_rental_system?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("MySQL Driver not found!");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Get a connection to the database.
+     * 
+     * @return Connection object
+     * @throws SQLException if connection fails
+     */
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
